74eca997772bd5c6a18e97cc66e44e322760f3b3
flysamc <flysamc@gmail.com>
2019-05-11 02:52:22 +0530

    remove api keys

--- venv/TwitterMain.py
+++ venv/TwitterMain.py
@@ -1,13 +1,10 @@
 import tweepy
 from tweepy import OAuthHandler
+from apisecret import CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_SECRET
 
-consumer_key = 'QEBCYVQZCyqE3H3WteTs7BNP9'
-consumer_secret = 'bd4GdAs8fVqAKtNkyRILQ4DIV7NWdXH5MxwB7Zzeyq9JSFbqJJ'
-access_token = '1059703438559240192-Qk4m9A9VCxVGwjwulXPLkDZOaXuZTQ'
-access_secret = 'zRO4v6lcyNI8AlrrPiQ4Z7MJ7OP4ID6d9Dqmwa82XOxJc'
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
 api = tweepy.API(auth)
 
